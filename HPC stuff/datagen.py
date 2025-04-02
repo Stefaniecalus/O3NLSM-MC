9b8069114f220d78cdcbc2aa3453a056f8d2e019
Boris De Vos <boris.devos@ugent.be>
2025-04-02 08:49:59 +0200

    clean up

--- HPC stuff/datagen.py
+++ HPC stuff/datagen.py
@@ -1,11 +1,8 @@
 import numpy as np
 
 # number of spins in lattice
-# 1225 2673 4961 8281 12825 18785
 Ls = [(6,1225),(8,2673),(10,4961),(12,8281),(14,12825),(16,18785)]
-
-
-Js = [0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 1.5, 1.7]
+Js = np.linspace(0.0, 1.4, 15, endpoint=True, dtype=float)
 steps_per_spin = 20000
 
 data = []
@@ -15,25 +12,3 @@ for (L, nspins) in Ls:
 
 data
 np.savetxt("HPC stuff/mcvalues.csv", data, delimiter=",", header="L,J,nsteps", fmt="%d,%0.1f,%d")
-
-
-from func_flips import *
-import time 
-def time_estim(steps=1000, L=6, J=0.5):
-    acceptance = [0,0,0]
-    nref = vec()
-    lattice = initial_lattice(L)
-    e = energy(lattice, J)
-    start = time.time()
-    for _ in range(steps):
-        e = metropolis_step(lattice, nref, J, acceptance, e)
-    end = time.time()
-
-    return (end-start)/steps
-
-times = []
-for L in [14, 16]:
-    print(L)
-    times.append(time_estim(steps=1000, L=L, J=0.5))
-times
-np.array(times)*1000
--- timing_tests.py
+++ timing_tests.py
@@ -1,70 +1,45 @@
 
 from func_flips import *
 import time 
-steps = 100
-L = 6
+steps = 1000
+L = 16
 nref = vec()
 J = 0.3
 acceptance = [0,0,0]
-lattice = initial_lattice(L, nref)
+lattice = initial_lattice(L)
 lat_dic, lat_coords, spinvalues = lattice
+len(lat_coords)
+20000*8281*0.07053/3600
+start = time.time()
 e = energy(lattice, J)
+end = time.time()
+print(end-start)
 
 start = time.time()
 for i in range(steps):
     e = metropolis_step(lattice, nref, J, acceptance, e)
-    # lat_dic, lat_coords, spinvalues = lattice
-    # old_energy = energy(lattice, J)
-
-    # #Define cone to give the flipped value a little nudge to avoid singularities
-    # dx, dy = random.uniform(-np.pi/4, np.pi/4), random.uniform(-np.pi/4, np.pi/4)
-    
-    # #now pick random coord from lat_coords to flip 
-    # flipcoord = random.choice(lat_coords)
-    # OG = spinvalues[lat_coords.index(flipcoord)]
-    # flip_values(lat_coords, spinvalues, flipcoord, [dx, dy, 0])
-
-    # #look to which cube this spin belongs to
-    # cubes = get_cubes(lat_dic, flipcoord)
-    
-    # #first look if this new configuration respects the hedgehog constraint
-    # checks = []
-    # for cube in cubes:
-    #     update_flux(lattice, cube, nref)
-    #     checks += [check_isolation(lat_dic, cube, nref)]
-      
-    # if np.sum(checks)==len(checks):
-    #     #if the first contraint is respected now calculate the probability of acception
-    #     new_energy = energy(lattice, J)
-    #     dE = new_energy - old_energy
-
-    #     if dE < 0 or np.random.rand() > np.exp(-dE):
-    #         #If the Metropolis step is accepted we only still need to flip the dictionary value
-    #         flip_dic(lat_dic, flipcoord, [dx, dy, 0])
-    #         acceptance += [2]
-        
-    #     else:
-    #         #If the Metropolis step is not accepted we need to flip the spinvalue and flux back to the old values
-    #         spinvalues[lat_coords.index(flipcoord)] = OG
-    #         for cube in cubes: update_flux(lattice, cube, nref)
-    #         acceptance += [1]
-            
-            
-    # else:
-    #     #If the hedgehog constrained is not accepted we need to flip the spinvalue and flux back to the old values
-    #     spinvalues[lat_coords.index(flipcoord)] = OG
-    #     for cube in cubes: update_flux(lattice, cube, nref)
-    #     acceptance += [0] 
-
 end = time.time()
 print((end-start)/steps)
+print(end-start)
 
-# for L = 6
-# time for initial_lattice = 0.4643947696685791s
-# time for metropolis_step = 1.3-1.7s
-# energy function: 0.42-0.45s -> done twice in metropolis_step
-# everything except energy calcs: 0.01-0.02s
+def time_estim(steps=1000, L=6, J=0.5):
+    acceptance = [0,0,0]
+    nref = vec()
+    lattice = initial_lattice(L)
+    e = energy(lattice, J)
+    start = time.time()
+    for _ in range(steps):
+        e = metropolis_step(lattice, nref, J, acceptance, e)
+    end = time.time()
 
+    return (end-start)/steps
+
+times = []
+for L in [14, 16]:
+    print(L)
+    times.append(time_estim(steps=1000, L=L, J=0.5))
+times
+np.array(times)*1000
 
 def metropolis_steps(L, nref, J, steps):
     acceptance = []
@@ -149,3 +124,22 @@ print((end-start)/steps)
 for coordinate in lat_coords:
     neighbors = spin_neighbours(coordinate, len(lat_dic)**(1/3))
     print(neighbors)
+
+
+# testing change_energy
+
+L=6
+nref = vec()
+J = 0.3
+steps = 10000
+lattice = initial_lattice(L, nref)
+lat_dic, lat_coords, spinvalues = lattice
+start = time.time()
+for i in range(steps):
+    flipcoord = random.choice(lat_coords)
+    e=change_energy(lattice, flipcoord, J)
+    cone = get_cone(lat_coords, spinvalues, flipcoord, nref, len(lat_dic)**(1/3))
+    flip_values(lat_coords, spinvalues, flipcoord, cone)
+    E_added = change_energy(lattice, flipcoord, J)
+end = time.time()
+print((end-start)/steps)
