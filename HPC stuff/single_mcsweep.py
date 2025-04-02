d749909eab97968b76a9213526d0f35efd4ab41a
Boris De Vos <boris.devos@ugent.be>
2025-04-02 17:22:45 +0200

    add quantities of interest to output

--- HPC stuff/single_mcsweep.py
+++ HPC stuff/single_mcsweep.py
@@ -29,11 +29,16 @@ print(L, J, nsteps)
 ##################################################################
 nref = vec()
 E, M, acceptance = MCS(L, nref, J, nsteps, nth)
-m = np.sum(M)/(nth*nspin)
+M_av = np.mean(M)
+M_av_squared = np.mean(M**2)
+M_av_quartic = np.mean(M**4)
+binder_cumulant = M_av_quartic / (M_av_squared**2) 
+m_density = M_av / (nspin)
 
 # write out data
-E,m,acceptance = np.float64(-349.2040644852037),np.float64(0.7518431151459216),[50,1000,23]
-data = np.array([E, m, acceptance[0], acceptance[1], acceptance[2]])
+E,m_density,acceptance = np.float64(-349.2040644852037),np.float64(0.7518431151459216),[50,1000,23]
+binder_cumulant = np.float64(1.5226894958649473)
+data = np.array([E, m_density, binder_cumulant, acceptance[0], acceptance[1], acceptance[2]])
 reshaped_data = data.reshape(1, data.shape[0])
-formatter = "%1f %1f %d %d %d"
+formatter = "%1f %1f %1f %d %d %d"
 np.savetxt("HPC stuff/test_output.txt", reshaped_data, fmt=formatter)
